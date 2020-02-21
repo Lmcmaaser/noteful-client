18f3b1050a86a38bdcf017c153b038e7f49aa1c7
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-02-21 12:53:29 -0700

    removed HasError

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -6,7 +6,6 @@ import ApiContext from '../ApiContext'
 import config from '../config'
 import PropTypes from 'prop-types';
 import './Note.css'
-import HasError from '../HasError'
 
 class Note extends React.Component {
   static defaultProps = {
@@ -46,13 +45,11 @@ class Note extends React.Component {
     const { name, id, modified } = this.props
     return (
       <div className='Note'>
-        <HasError>
           <h2 className='Note__title'>
             <Link to={`/note/${id}`}>
               {name}
             </Link>
           </h2>
-        </HasError>
         <button
           className='Note__delete'
           type='button'
@@ -63,7 +60,6 @@ class Note extends React.Component {
           remove
         </button>
         <div className='Note__dates'>
-          <HasError>
             <div className='Note__dates-modified'>
               Modified
               {' '}
@@ -71,7 +67,6 @@ class Note extends React.Component {
                 {format(modified, 'Do MMM YYYY')}
               </span>
             </div>
-          </HasError>
         </div>
       </div>
     )
